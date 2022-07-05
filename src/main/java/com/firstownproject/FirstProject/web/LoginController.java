d1a743757deb5e84f2ecb2ba0022251e65e3ae9b
borislav <byborisov@icloud.com>
2022-07-06 00:47:48 +0300

    Moved login to templates, since I decided to use thymeleaf for it. Manged to get it running. PS. Bootstrap is still annoying.

--- src/main/java/com/firstownproject/FirstProject/web/LoginController.java
+++ src/main/java/com/firstownproject/FirstProject/web/LoginController.java
@@ -1,10 +1,16 @@
 package com.firstownproject.FirstProject.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping
+    public String loginPage() {
+        return "login";
+    }
 }
